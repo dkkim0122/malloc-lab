cc53157fe92e9bc393a4ea9dd277ad9c02a22ecc
dkkim0122 <dkkim0122@gmail.com>
2021-12-11 15:45:43 +0900

    remarked memlib.c & basic constants and macros in mm.c

--- memlib.c
+++ memlib.c
@@ -21,10 +21,12 @@ static char *mem_max_addr;   /* largest legal heap address */
 
 /* 
  * mem_init - initialize the memory system model
+
  */
 void mem_init(void)
 {
     /* allocate the storage we will use to model the available VM */
+    // config.h에 정의되어 있음, #define MAX_HEAP (20*(1<<20)) : 20971520bytes == 20 MB
     if ((mem_start_brk = (char *)malloc(MAX_HEAP)) == NULL) {
 	fprintf(stderr, "mem_init_vm: malloc error\n");
 	exit(1);
@@ -57,15 +59,20 @@ void mem_reset_brk()
  */
 void *mem_sbrk(int incr) 
 {
-    char *old_brk = mem_brk;
+    char *old_brk = mem_brk;  // 힙 늘이기 전의 끝 포인터를 저장한다.
 
+    /*
+        힙이 줄어들거나 최대 힙 사이즈를 벗어난다면
+        메모리 부족으로 에러처리 하고 -1을 리턴한다.
+    */
     if ( (incr < 0) || ((mem_brk + incr) > mem_max_addr)) {
-	errno = ENOMEM;
+	errno = ENOMEM;  // 메모리 부족 에러 처리
 	fprintf(stderr, "ERROR: mem_sbrk failed. Ran out of memory...\n");
-	return (void *)-1;
+	return (void *)-1;  // 리턴값이 void*여야 해서 형변환 해준 듯
     }
-    mem_brk += incr;
-    return (void *)old_brk;
+    
+    mem_brk += incr; // mem_brk에 incr만큼 더함으로써 힙을 늘려주었다.
+    return (void *)old_brk;  // 이전 brk를 리턴한다. 왜? 이 새로운 메모리를 처음부터 사용해야 하니까.
 }
 
 /*
--- mm.c
+++ mm.c
@@ -24,11 +24,11 @@
  ********************************************************/
 team_t team = {
     /* Team name */
-    "ateam",
+    "team 9",
     /* First member's full name */
-    "Harry Bovik",
+    "Kim Dokyung",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+    "dkkim0122@gmail.com",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
@@ -39,10 +39,55 @@ team_t team = {
 #define ALIGNMENT 8
 
 /* rounds up to the nearest multiple of ALIGNMENT */
+// size보다 큰 가장 가까운 ALIGNMENT의 배수로 만들어준다 -> 정렬!
+// size = 7 : (00000111 + 00000111) & 11111000 = 00001110 & 11111000 = 00001000 = 8
+// size = 13 : (00001101 + 00000111) & 11111000 = 00010000 = 16
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
-
+/* 메모리 할당 시 기본적으로 header와 footer를 위해 필요한 더블워드만큼의 메모리 크기 */
+// size_t : 해당 시스템에서 어떤 객체나 값이 포함할 수 있는 최대 크기의 데이터
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+
+/*
+    기본 상수와 매크로
+*/
+
+/* 기본 단위인 word, double word, 새로 할당받는 힙의 크기 CHUNKSIZE를 정의한다 */
+#define WSIZE       4       /* Word and header/footer size (bytes) */
+#define DSIZE       8       /* Double word size (bytes) */
+#define CHUNKSIZE   (1<<12) /* Extend heap by this amount : 4096bytes -> 4kib */
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))  // 최댓값 구하는 함수 매크로
+
+/* header 및 footer 값(size + allocated) 리턴 */
+// 더블워드 정렬로 인해 size의 오른쪽 3~4자리는 비어 있다. 
+//이 곳에 0(freed), 1(allocated) flag를 삽입한다.
+#define PACK(size, alloc)   ((size | (alloc))   
+
+/* 주소 p에서의 word를 읽어오거나 쓰는 함수 */
+// 포인터 p가 가리키는 주소의 값을 리턴하거나 val로 변경
+#define GET(p)          (*(unsigned int*)(p))
+#define PUT(p, val)     (*(unsinged int*)(p) = val)
+
+/* header or footer에서 블록의 size, allocated field를 읽어온다 */
+// & ~0x7 => 0x7:0000 0111 ~0x7:1111 1000이므로 ex. 1011 0111 & 1111 1000 = 1011 0000 : size 176bytes
+// & 0x1 => ex. 1011 0111 | 0000 0001 = 1 : Allocated!
+#define GET_SIZE(p)     (GET(p) & ~0x7) 
+#define GET_ALLOC(p)    (GET(p) & 0x1)    
+
+/* 블록 포인터 bp를 인자로 받아 블록의 header와 footer의 주소를 반환한다 */
+// 포인터가 char* 형이므로, 숫자를 더하거나 빼면 그 만큼의 바이트를 뺀 것과 같다.
+// WSIZE 4를 뺀다는 것은 주소가 4byte(1 word) 뒤로 간다는 뜻. bp의 1word 뒤는 헤더.
+#define HDRP(bp)    ((char*)(bp) - WSIZE) 
+#define FTRP(bp)    ((char*)(bp) + GET_SIZE(bp) - DSIZE)
+
+/* 블록 포인터 bp를 인자로 받아 이후, 이전 블록의 주소를 리턴한다 */
+// 지금 블록의 bp에 블록의 크기(char*이므로 word단위)만큼을 더한다.
+// 지금 블록의 bp에 이전 블록의 footer에서 참조한 이전 블록의 크기를 뺀다.
+#define NEXT_BLKP(bp)   ((char*)(bp) + GET_SIZE((char*)bp - WSIZE)) // (char*)(bp) + GET_SIZE(지금 블록의 헤더값)
+#define PREV_BLKP(bp)   ((char*)(bp) - GET_SIZE((char*)bp - DSIZE)) // (char*)(bp) - GET_SIZE(이전 블록의 풋터값)
+
+/*===================================================================*/
 
 /* 
  * mm_init - initialize the malloc package.
