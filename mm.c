df840f44012d372353abc3cb06b8156926579cd6
dkkim0122 <dkkim0122@gmail.com>
2021-12-14 11:29:48 +0900

    Implicit & first, next-fit, removed unnecessary comments

--- mm.c
+++ mm.c
@@ -41,16 +41,9 @@ team_t team = {
 #define ALIGNMENT 8
 
 /* rounds up to the nearest multiple of ALIGNMENT */
-// size보다 큰 가장 가까운 ALIGNMENT의 배수로 만들어준다 -> 정렬!
-// size = 7 : (00000111 + 00000111) & 11111000 = 00001110 & 11111000 = 00001000 = 8
-// size = 13 : (00001101 + 00000111) & 11111000 = 00010000 = 16
-// 1 ~ 7 bytes : 8 bytes
-// 8 ~ 16 bytes : 16 bytes
-// 17 ~ 24 bytes : 24 bytes
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
 /* 메모리 할당 시 기본적으로 header와 footer를 위해 필요한 더블워드만큼의 메모리 크기 */
-// size_t : 해당 시스템에서 어떤 객체나 값이 포함할 수 있는 최대 크기의 데이터
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
 /*
@@ -65,30 +58,21 @@ team_t team = {
 #define MAX(x, y) ((x) > (y) ? (x) : (y))  // 최댓값 구하는 함수 매크로
 
 /* header 및 footer 값(size + allocated) 리턴 */
-// 더블워드 정렬로 인해 size의 오른쪽 3~4자리는 비어 있다. 
-//이 곳에 0(freed), 1(allocated) flag를 삽입한다.
 #define PACK(size, alloc)   ((size) | (alloc))   
 
 /* 주소 p에서의 word를 읽어오거나 쓰는 함수 */
-// 포인터 p가 가리키는 곳의 값을 리턴하거나 val을 저장
 #define GET(p)          (*(unsigned int*)(p))
 #define PUT(p, val)     (*(unsigned int*)(p) = (val))
 
 /* header or footer에서 블록의 size, allocated field를 읽어온다 */
-// & ~0x7 => 0x7:0000 0111 ~0x7:1111 1000이므로 ex. 1011 0111 & 1111 1000 = 1011 0000 : size 176bytes
-// & 0x1 => ex. 1011 0111 | 0000 0001 = 1 : Allocated!
 #define GET_SIZE(p)     (GET(p) & ~0x7) 
 #define GET_ALLOC(p)    (GET(p) & 0x1)    
 
 /* 블록 포인터 bp를 인자로 받아 블록의 header와 footer의 주소를 반환한다 */
-// 포인터가 char* 형이므로, 숫자를 더하거나 빼면 그 만큼의 바이트를 뺀 것과 같다.
-// WSIZE 4를 뺀다는 것은 주소가 4byte(1 word) 뒤로 간다는 뜻. bp의 1word 뒤는 헤더.
 #define HDRP(bp)    ((char*)(bp) - WSIZE) 
 #define FTRP(bp)    ((char*)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
 
 /* 블록 포인터 bp를 인자로 받아 이후, 이전 블록의 주소를 리턴한다 */
-// NEXT : 지금 블록의 bp에 블록의 크기(char*이므로 word단위)만큼을 더한다.
-// PREV : 지금 블록의 bp에 이전 블록의 footer에서 참조한 이전 블록의 크기를 뺀다.
 #define NEXT_BLKP(bp)   ((char*)(bp) + GET_SIZE(((char*)(bp) - WSIZE))) // (char*)(bp) + GET_SIZE(지금 블록의 헤더값)
 #define PREV_BLKP(bp)   ((char*)(bp) - GET_SIZE(((char*)(bp) - DSIZE))) // (char*)(bp) - GET_SIZE(이전 블록의 풋터값)
 
@@ -112,7 +96,6 @@ void *mm_malloc(size_t size);
 void mm_free(void *bp);
 void *mm_realloc(void *ptr, size_t size);
 
-/*===================================================================*/
 
 /* 
  * mm_init - initialize the malloc package.
@@ -120,13 +103,9 @@ void *mm_realloc(void *ptr, size_t size);
 int mm_init(void)
 {
     /* 메모리에서 4word 가져오고 이걸로 빈 가용 리스트 초기화 */
-    // 먼저 4word 크기의 메모리를 불러온다. heap_listp가 그 첫 주소를 가리킨다.
     if ((heap_listp = mem_sbrk(4*WSIZE)) == (void*)-1)
         return -1;
 
-    // 맨 앞의 1워드는 정보 0인 미사용 패딩.
-    // 그 다음의 prologue block은 header와 footer로만 이루어진 2word짜리 블록
-    // 맨 뒤는 헤더만으로 구성된 크기 0의 epilogue block
     PUT(heap_listp, 0);  // Alignment padding. 더블 워드 경계로 정렬된 미사용 패딩.
     PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));  // prologue header
     PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));  // prologue footer
@@ -138,7 +117,6 @@ int mm_init(void)
     #endif
 
     /* 그 후 CHUNKSIZE만큼 힙을 확장해 초기 가용 블록을 생성한다. */
-    // 새 힙을 CHUNKSIZE 바이트만큼의 WORD 개수만큼 늘려준다.
     if (extend_heap(CHUNKSIZE / WSIZE) == NULL) //실패하면 -1 리턴
         return -1;
 
@@ -146,8 +124,7 @@ int mm_init(void)
 }
 
 /*
-    coalesce(bp) : 해당 가용 블록을 앞뒤 가용 블록과 연결하고 
-    연결된 가용 블록의 주소를 리턴한다.
+    coalesce(bp) : 해당 가용 블록을 앞뒤 가용 블록과 연결하고 연결된 가용 블록의 주소를 리턴한다.
 */
 static void* coalesce(void* bp){
     // 직전 블록의 footer, 직후 블록의 header를 보고 가용 여부를 확인.
@@ -156,7 +133,6 @@ static void* coalesce(void* bp){
     size_t size = GET_SIZE(HDRP(bp));
 
     // case 1 : 직전, 직후 블록이 모두 할당
-    // 해당 블록만.
     if (prev_alloc && next_alloc)
         return bp;
 
@@ -202,7 +178,6 @@ static void* extend_heap(size_t words){ // 워드 단위로 받는다.
     size_t size;
     
     /* 더블 워드 정렬에 따라 메모리를 mem_sbrk 함수를 이용해 할당받는다. */
-    // Double Word Alignment : 늘 짝수 개수의 워드를 할당해주어야 한다.
     size = (words % 2) ? (words + 1) * WSIZE : (words) * WSIZE; // size를 짝수 word && byte 형태로 만든다.
     if ((long)(bp = mem_sbrk(size)) == -1) // 새 메모리의 첫 부분을 bp로 둔다. 주소값은 int로는 못 받아서 long으로 casting한 듯.
         return NULL;
@@ -240,12 +215,10 @@ void *mm_malloc(size_t size)
         return bp;
     }
 
-    // 만약 맞는 크기의 가용 블록이 없다면 새로 힙을 늘려서 
+    // 만약 맞는 크기의 가용 블록이 없다면 새로 힙을 늘려서 새 힙에 메모리를 할당한다.
     extendsize = MAX(asize, CHUNKSIZE);  // 둘 중 더 큰 값으로 사이즈를 정한다.
-    // extend_heap()은 word 단위로 인자를 받으므로 WSIZE로 나눠준다.
     if ((bp = extend_heap(extendsize / WSIZE)) == NULL) 
         return NULL;
-    // 새 힙에 메모리를 할당한다.
     place(bp, asize);
     return bp;
 }
@@ -266,11 +239,7 @@ static void* find_fit(size_t asize){
                 return bp;
         }
 
-        /*
-        만약 끝까지 찾았는데도 안 나왔으면 처음부터 찾아본다.
-        이 구문이 없으면 바로 extend_heap을 하는데, 
-        이럼 앞에 있는 가용 블록들을 사용하지 못할 수 있어 메모리 낭비이다.
-        */
+        // 만약 끝까지 찾았는데도 안 나왔으면 처음부터 찾아본다.
         for (bp = heap_listp; bp < old_last_freep; bp = NEXT_BLKP(bp)){
             if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp))))
                 return bp;
@@ -285,7 +254,6 @@ static void* find_fit(size_t asize){
         void* bp;
 
         // 프롤로그 블록에서 에필로그 블록 전까지 블록 포인터 bp를 탐색한다.
-        // 블록이 가용 상태이고 사이즈가 요구 사이즈보다 크다면 해당 블록 포인터를 리턴
         for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)){
             if(!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))){
                 return bp;
@@ -308,8 +276,6 @@ static void place(void* bp, size_t asize){
     size_t csize = GET_SIZE(HDRP(bp));
 
     // 분할이 가능한 경우
-    // -> 남은 메모리가 최소한의 가용 블록을 만들 수 있는 4word(16byte)가 되느냐.
-    // header & footer : 1word씩, payload : 1word, 정렬 위한 padding : 1word = 4words
     if ((csize - asize) >= (2*DSIZE)){
         // 앞의 블록은 할당 블록으로
         PUT(HDRP(bp), PACK(asize, 1));
@@ -360,8 +326,7 @@ void *mm_realloc(void *ptr, size_t size)
     // copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
     copySize = GET_SIZE(HDRP(oldptr));
 
-    // 원래 메모리 크기보다 적은 크기를 realloc하면 
-    // 크기에 맞는 메모리만 할당되고 나머지는 안 된다. 
+    // 원래 메모리 크기보다 적은 크기를 realloc하면 크기에 맞는 메모리만 할당되고 나머지는 안 된다. 
     if (size < copySize)
       copySize = size;
 
